ed7ea10fec7dc3fec6da32f1439c356cd84021e4
ivmaeo <ivmaeo@ttu.ee>
2023-09-29 14:16:03 +0300

    tsconfig, axios, baseService

--- src/axios/avaandmed.ts
+++ src/axios/avaandmed.ts
@@ -1,13 +1,13 @@
 import axios, { type AxiosInstance } from "axios";
 
-export function avaandmeDatasetFile(dataset: string, fileId: string, requestMethod: string): AxiosInstance {
+export function avaandmeDatasetFile(dataset: string, fileId: string): AxiosInstance {
     return axios.create({
         baseURL: `https://avaandmed.eesti.ee/api/`,
         url: `datasets/${dataset}/files/${fileId}/download`,
-        method: requestMethod,
+        method: 'post',
         timeout: 1000,
         headers: {
-            "Content-type": "application/json"
+            'Content-type': 'application/json'
         }
     });
 }
